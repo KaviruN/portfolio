d148411ff1022048a6e7ce10ffe13718d98476dc
Kaviru(UBUNTU) <kavirunethsara1@outlook.com>
2025-06-07 14:01:02 +0530

    feat: add SkillCard component and integrate skills section in App; update styles for skills in _skills.scss; modify index.scss to forward skills styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import 'remixicon/fonts/remixicon.css'
 import "./scss/style.css";
 import Hero from './Hero';
 import NavBar from "./NavBar"
+import SkillCard from './SkillCard';
+import 'devicon/devicon.min.css'
 
 
 function App() {
@@ -11,6 +13,23 @@ function App() {
         <NavBar />
         <Hero />
       </header>
+      <main>
+        <section className='skills'>
+          <SkillCard icon="ri-html5-fill" name="HTML"/>
+          <SkillCard icon="ri-css3-fill" name="css"/>
+          <SkillCard icon="ri-javascript-fill" name="JvaScript"/>
+          <SkillCard icon="devicon-typescript-plain" name="TypeScript"/>
+          <SkillCard icon="ri-reactjs-line" name="React"/>
+          <SkillCard icon="devicon-sass-original" name="Sass(Scss)"/>
+          <SkillCard icon="devicon-git-plain" name="Git"/>
+          <SkillCard icon="ri-github-fill" name="GitHub"/>
+          <SkillCard icon="devicon-c-plain" name="C"/>
+          <SkillCard icon="devicon-python-plain" name="Python"/>
+          <SkillCard icon="devicon-django-plain" name="Django"/>
+          <SkillCard icon="devicon-djangorest-plain" name="DjangoReset"/>
+          
+        </section>
+      </main>
     </div>
   )
 }
