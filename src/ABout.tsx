0379c3151784fbdced5176a23dd002d2eef9ee0c
Kaviru(UBUNTU) <kavirunethsara1@outlook.com>
2025-06-07 14:35:31 +0530

    feat: enhance App component with About section and Skills display; update styles for header, hero, and skills components

---
+++ src/ABout.tsx
@@ -0,0 +1,12 @@
+const About = () => {
+    return (
+        <div className="about">
+        <h1>About This App</h1>
+        <p>This app is designed to help you manage your tasks efficiently.</p>
+        <p>It allows you to create, edit, and delete tasks, as well as mark them as completed.</p>
+        <p>We hope you find it useful!</p>
+        </div>
+    );
+}
+
+export default About;
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import 'remixicon/fonts/remixicon.css'
+import 'devicon/devicon.min.css'
 import "./scss/style.css";
 import Hero from './Hero';
 import NavBar from "./NavBar"
 import SkillCard from './SkillCard';
-import 'devicon/devicon.min.css'
+import About from './ABout';
+
 
 
 function App() {
@@ -14,20 +16,24 @@ function App() {
         <Hero />
       </header>
       <main>
+        <About />
         <section className='skills'>
-          <SkillCard icon="ri-html5-fill" name="HTML"/>
-          <SkillCard icon="ri-css3-fill" name="css"/>
-          <SkillCard icon="ri-javascript-fill" name="JvaScript"/>
-          <SkillCard icon="devicon-typescript-plain" name="TypeScript"/>
-          <SkillCard icon="ri-reactjs-line" name="React"/>
-          <SkillCard icon="devicon-sass-original" name="Sass(Scss)"/>
-          <SkillCard icon="devicon-git-plain" name="Git"/>
-          <SkillCard icon="ri-github-fill" name="GitHub"/>
-          <SkillCard icon="devicon-c-plain" name="C"/>
-          <SkillCard icon="devicon-python-plain" name="Python"/>
-          <SkillCard icon="devicon-django-plain" name="Django"/>
-          <SkillCard icon="devicon-djangorest-plain" name="DjangoReset"/>
-          
+          <h2 className='skills__title'>My Skills</h2>
+          <div className="skills__cards">
+            <SkillCard icon="ri-html5-fill" name="HTML" />
+            <SkillCard icon="ri-css3-fill" name="css" />
+            <SkillCard icon="ri-javascript-fill" name="JvaScript" />
+            <SkillCard icon="devicon-typescript-plain" name="TypeScript" />
+            <SkillCard icon="ri-reactjs-line" name="React" />
+            <SkillCard icon="devicon-sass-original" name="Sass(Scss)" />
+            <SkillCard icon="devicon-git-plain" name="Git" />
+            <SkillCard icon="ri-github-fill" name="GitHub" />
+            <SkillCard icon="devicon-c-plain" name="C" />
+            <SkillCard icon="devicon-python-plain" name="Python" />
+            <SkillCard icon="devicon-django-plain" name="Django" />
+            <SkillCard icon="devicon-djangorest-plain" name="DjangoReset" />
+          </div>
+
         </section>
       </main>
     </div>
