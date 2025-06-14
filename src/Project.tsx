82303fb2fe85bb0e3d469bc0a8c1b038c7440676
Kaviru(UBUNTU) <kavirunethsara1@outlook.com>
2025-06-14 11:52:06 +0530

    Implement code changes to enhance functionality and improve performance

--- src/Project.tsx
+++ src/Project.tsx
@@ -1,3 +1,6 @@
+import nexcent from './assets/nexcent.png';
+
+
 type ProjectProps = {
     title: string;
     description: string;
@@ -6,9 +9,17 @@ type ProjectProps = {
 function Project({ title, description }: ProjectProps) {
     return (
         <div className="projects__project">
-            <h2 className="projects__title">{title}</h2>
-            <div className="projects__description">
-                <p className="projects__text">{description}</p>
+            <div className="projects__header">
+                <h2 className="projects__title">{title}</h2>
+                <div className="projects__description">
+                    <p className="projects__text">{description}</p>
+                </div>
+            </div>
+            <div className="projects__view">
+                <div className="projects__view--background"></div>
+                <div className="projects__view--overlay">
+                    <a href="https://kavirun.github.io/nexcent/"><img src={nexcent} alt="nexcent" /></a>
+                </div>
             </div>
         </div>
     );
