a5f2e4c46a6c9c275f4e4cd89b805a829b5e1461
Kaviru(UBUNTU) <kavirunethsara1@outlook.com>
2025-06-14 20:07:03 +0530

    feat: update Project and Projects components to include image and link props; enhance styles for better layout

--- src/Project.tsx
+++ src/Project.tsx
@@ -1,12 +1,13 @@
-import nexcent from './assets/nexcent.png';
 
 
 type ProjectProps = {
     title: string;
     description: string;
+    img: string;
+    link: string;
 };
 
-function Project({ title, description }: ProjectProps) {
+function Project({ title, description, img, link }: ProjectProps) {
     return (
         <div className="projects__project">
             <div className="projects__header">
@@ -16,9 +17,9 @@ function Project({ title, description }: ProjectProps) {
                 </div>
             </div>
             <div className="projects__view">
-                <div className="projects__view--background"></div>
-                <div className="projects__view--overlay">
-                    <a href="https://kavirun.github.io/nexcent/"><img src={nexcent} alt="nexcent" /></a>
+                {/* <div className="projects__view--background"></div> */}
+                <div className="projects__img">
+                    <a href={link}><img src={img} alt="nexcent" /></a>
                 </div>
             </div>
         </div>
--- src/Projects.tsx
+++ src/Projects.tsx
@@ -1,10 +1,11 @@
 import Project from './Project';
+import nexcent from './assets/nexcent.png';
 
 
 function Projects( ) {
     return (
         <section className="projects">
-            <Project title="nexcent" description="The Nexcent project is a front-end application built using React and TypeScript, inspired by a free Figma template. It aims to transform design concepts into a functional and user-friendly interface." />
+            <Project link='https://kavirun.github.io/nexcent/' img={nexcent} title="nexcent" description="The Nexcent project is a front-end application built using React and TypeScript, inspired by a free Figma template. It aims to transform design concepts into a functional and user-friendly interface." />
         </section>
     )
 }
