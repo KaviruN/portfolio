7eb6572bb195ab2437370407aa3f093a5f0e452a
Kaviru(UBUNTU) <kavirunethsara1@outlook.com>
2025-06-14 22:04:30 +0530

    style: enhance Projects component layout and responsiveness; add additional project entry and adjust SCSS styles for better alignment

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -7,6 +7,7 @@ function Projects( ) {
         <section className="projects">
             <Project link='https://kavirun.github.io/nexcent/' img={nexcent} title="nexcent" description="The Nexcent project is a front-end application built using React and TypeScript, inspired by a free Figma template. It aims to transform design concepts into a functional and user-friendly interface." />
             <Project link='https://kavirun.github.io/nexcent/' img={nexcent} title="nexcent" description="The Nexcent project is a front-end application built using React and TypeScript, inspired by a free Figma template. It aims to transform design concepts into a functional and user-friendly interface." />
+            <Project link='https://kavirun.github.io/nexcent/' img={nexcent} title="nexcent" description="The Nexcent project is a front-end application built using React and TypeScript, inspired by a free Figma template. It aims to transform design concepts into a functional and user-friendly interface." />
         </section>
     )
 }
