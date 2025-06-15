4d4d892391f5ce4e1c7b09247acca69ca1390575
Kaviru(UBUNTU) <kavirunethsara1@outlook.com>
2025-06-15 18:56:25 +0530

    style: add 'sub' prop to Project component and display it in the header; update project entries in Projects component to include sub titles

--- src/Project.tsx
+++ src/Project.tsx
@@ -2,15 +2,17 @@
 
 type ProjectProps = {
     title: string;
+    sub: string;
     description: string;
     img: string;
     link: string;
 };
 
-function Project({ title, description, img, link }: ProjectProps) {
+function Project({ title, description, img, link, sub }: ProjectProps) {
     return (
         <div className="projects__project">
             <div className="projects__header">
+                <h4>{sub}</h4>
                 <h2 className="projects__title">{title}</h2>
                 <div className="projects__description">
                     <p className="projects__text">{description}</p>
