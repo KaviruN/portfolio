b438096cbf9ecca28d7f70510c31ccf35fd3a5e2
Kaviru(UBUNTU) <kavirunethsara1@outlook.com>
2025-06-15 19:36:45 +0530

    Implement code changes to enhance functionality and improve performance

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -1,13 +1,14 @@
 import Project from './Project';
 import nexcent from './assets/nexcent.png';
+import jakeElwood from './assets/jake&elwood.png';
 
 
 function Projects( ) {
     return (
         <section className="projects">
             <Project demo='https://projects.kaviru.me/nexcent/' githublink='https://github.com/KaviruN/nexcent' img={nexcent} title="nexcent" sub="Frontend Focuse" description="The Nexcent project is a front-end application built using React and TypeScript, inspired by a free Figma template. It aims to transform design concepts into a functional and user-friendly interface." />
-            <Project demo='https://kavirun.github.io/nexcent/'  githublink='https://github.com/KaviruN/nexcent' img={nexcent} title="nexcent" sub="Frontend Focuse" description="The Nexcent project is a front-end application built using React and TypeScript, inspired by a free Figma template. It aims to transform design concepts into a functional and user-friendly interface." />
-            <Project demo='https://kavirun.github.io/nexcent/' githublink='https://github.com/KaviruN/nexcent' img={nexcent} title="nexcent" sub="Frontend Focuse" description="The Nexcent project is a front-end application built using React and TypeScript, inspired by a free Figma template. It aims to transform design concepts into a functional and user-friendly interface." />
+            <Project demo='https://projects.kaviru.me/Jake-Elwood/'  githublink='https://github.com/KaviruN/Jake-Elwood' img={jakeElwood} title="Jake & Elwood" sub="Frontend Focuse" description="This repository contains my learning projects. It is a collection of simple projects and experiments created while learning new programming concepts and technologies." />
+            {/* <Project demo='' githublink='' img={} title="" sub="Frontend Focuse" description="" /> */}
         </section>
     )
 }
