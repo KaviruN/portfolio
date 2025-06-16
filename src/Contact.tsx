5de1940bc104fa7db12d704625a0cdaa2924581f
Kaviru(UBUNTU) <kavirunethsara1@outlook.com>
2025-06-16 21:43:46 +0530

    fix: correct order of social media links in Contact component

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -5,8 +5,8 @@ function Contact(){
             <p>I'm currently looking to join a cross-functional team that values improving people's lives through accessible technology. I'm also passionate about CTFs and cybersecurity challenges. Got a project or idea in mind? Let’s connect.</p>
             <a className="contact__email" href="mailto:contact@kaviru.me">contact@kaviru.me</a>
             <div className="contact__socials">
-                <a href="https://lk.linkedin.com/in/kaviru-n" target="_blank"><i className="ri-github-fill"></i></a>
-                <a href="https://github.com/KaviruN/" target="_blank"><i className="ri-linkedin-box-fill"></i></a>
+                <a href="https://github.com/KaviruN" target="_blank"><i className="ri-github-fill"></i></a>
+                <a href="https://lk.linkedin.com/in/kaviru-n" target="_blank"><i className="ri-linkedin-box-fill"></i></a>
             </div>
         </footer>
     )
