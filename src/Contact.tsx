88c7a0fbbe95447f9bccaddb9ee604c3f02be7a2
Kaviru(UBUNTU) <kavirunethsara1@outlook.com>
2025-06-16 21:38:53 +0530

    fix: add missing heading to Contact component

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -1,6 +1,7 @@
 function Contact(){
     return(
         <footer className="contact" id="contact">
+            <h3>Contact Me</h3>
             <p>I'm currently looking to join a cross-functional team that values improving people's lives through accessible technology. I'm also passionate about CTFs and cybersecurity challenges. Got a project or idea in mind? Let’s connect.</p>
             <a className="contact__email" href="mailto:contact@kaviru.me">contact@kaviru.me</a>
             <div className="contact__socials">
